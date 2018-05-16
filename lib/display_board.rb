754c4f1c1c94addd23effd48dd6b3ca3a88256ce
Kathy Nguyen <kathyvng@gmail.com>
2018-05-16 07:40:51 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,18 +1,10 @@
-board = [" "," "," "," "," "," "," "," "," "]
-def display_board (board)
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-end
-
+def display_board(
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+  puts 'display board'
 board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
-def display_board (board)
-  puts "   |   |   "
-  puts "-----------"
-  puts "   | X |   "
-  puts "-----------"
-  puts "   |   |   "
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
+puts "-----------"
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts "-----------"
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-
