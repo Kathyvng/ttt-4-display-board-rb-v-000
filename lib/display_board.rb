ea2bd7a1e9a21e90434eb6f456d23cf715a7c8b7
Kathy Nguyen <kathyvng@gmail.com>
2018-05-16 06:58:14 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,9 @@
 def display_board (board)
-
   board = [" "," "," "," "," "," "," "," "," "]
-  puts "#{board[0]}, #{board[1]}, #{board[2]}, #{board[3]}, #{board[4]}, #{board[5]}, #{board[6]}, #{board[7]}, #{board[8]}"
-
+  rows[0]= "   |   |   "
+  rows[1]= "-----------"
+  rows[2]= "   |   |   "
+  rows[3]= "-----------"
+  rows[4]= "   |   |   "
 end
 # out the current state.
